390afe3d2d0a3a05c625570e0282d9f6c909f598
deninising <906086497@qq.com>
2019-10-22 08:32:27 +0800

    修改

--- mysso/mysso-server-redis/src/main/java/com/dennis/mysso/server/redis/MyssoServerRedisApplication.java
+++ mysso/mysso-server-redis/src/main/java/com/dennis/mysso/server/redis/MyssoServerRedisApplication.java
@@ -3,7 +3,7 @@ package com.dennis.mysso.server.redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dennis.mysso.common")
 public class MyssoServerRedisApplication {
 
     public static void main(String[] args) {
